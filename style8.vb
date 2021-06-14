7e7ca7094677756cb1bc89b082cf5bce2ceecbaa
Shreya15-02 <76565705+Shreya15-02@users.noreply.github.com>
2021-06-15 02:25:32 +0530

    Add files via upload

---
+++ style8.vb
@@ -0,0 +1,24 @@
+.col {
+    float: left;
+    width: 33.33%;
+  }
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  .subscription{
+      background-color:whitesmoke	;
+  }
+
+  .content1{
+    border-right: 2px solid black;
+    text-decoration-line: none;
+    
+  }
+
+  .content2{
+    border-right: 2px solid black;
+  } 
+  
