6156a6ebdd1c1f57d465288f1456708dcd9496c0
Shreya15-02 <76565705+Shreya15-02@users.noreply.github.com>
2021-06-15 02:25:11 +0530

    Add files via upload

---
+++ CSS_ASSIGNMENTS_3.vb
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style8.css">
+</head>
+<body>
+    <br>
+    <hr style="height: 2px;  background-color: black ; color: black;">
+    <br>
+    <div class="row">
+        <div class="col" style="border-width: 2px;">
+            <table cellpadding='10px' class="content1">
+                <tr>
+                    <td>Connect With Us <img src="image/youtube.png" style="width: 30px; height: 30px;"> <img src="image/facebook.png" style="width: 30px; height: 30px;"> <img src="image/twitter.png" style="width: 30px; height: 30px;"></td>
+                </tr>
+                <tr>
+                    <td>Linux Journal, currently celebrating its 25th year of publication, is the original magazine of the global Open Source community.</td>
+                </tr>
+                <tr>
+                    <td>© 2019 Linux Journal, LLC. All rights reserved.</td>
+                </tr>
+                <tr>    
+                    <td id="decor" ><a href="#" style="color: black;">PRIVACY POLICY</a> | <a href="#" style="color: black;">TERMS OF SERVICE</a> | <a href="#" style="color: black;">ADVERTISE</a></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col">
+            <table cellpadding='10px' class="content2">
+                <tr>
+                    <td><button type="button" class="subscription">SUBSCRIBE</button></td>
+                    <td>MASTHEAD</td>
+                    <td>RSS FEEDS</td>
+                </tr>
+                <tr>
+                    <td>RENEW</td>
+                    <td>FAQ</td>
+                    <td>NEWSLETTERS</td>
+                </tr>
+                <tr>
+                    <td>BACKISSUES</td>
+                    <td>AUTHORS</td>
+                    <td>MERCHANDISE</td>
+                </tr>
+                <tr>
+                    <td>CUSTOMER <br>
+                         SERVICE</td>
+                    <td>LETTERS TO <br>
+                        EDITORS</td>
+                    <td>CONTACT US</td>
+                </tr>
+            </table>
+        </div>
+    
+    
+        <div class="col">
+            <table cellpadding='10px'>
+                <tr>
+                    <td><br></td>
+                </tr>
+                <tr>
+                    <td><br></td>
+                </tr>
+                <tr>
+                    <td style="font-weight:bold;">Powered By</td>
+                </tr>
+                <tr>
+                    <td><img src="image/linux.jpg" style="width: 80%;"></td>
+                </tr>
+    
+            </table>
+        </div>
+      </div>
+    
+    
+    </BODY>
+    </HTML>  
+</body>
+</html>
